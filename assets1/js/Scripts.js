649a359b3b11794982b5b32274e55458b9e7ef35
eliasmunizrj <108908212+eliasmunizrj@users.noreply.github.com>
2023-04-26 14:31:40 -0300

    teste

---
+++ assets1/js/Scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { 
+
+    
+    setTimeout(function() { 
+        $('#botaocheckout').show(); 
+        $('#comprasegura').show();
+        //$('#elonmusk').show();
+        //$('#composicao').show();
+        //$('#depoimentos').show();
+        $('#depoimentos-botao').show();
+        $('#ofertas').show();
+        //$('#faq').show();
+        $('#formas-pagamento').show();
+
+ }, 01*1000); 
+});
